54b8608aee00e56505a5fc67371134e344b95b84
Andrea Valenzi <andrew.valenzi@gmail.com>
2018-10-14 22:22:29 +0200

    - handled db error; - transaction amount rounded;

--- src/Models/transactionModel.php
+++ src/Models/transactionModel.php
@@ -1,30 +1,40 @@
 <?php
 class TransactionModel {
     function __construct(){
-      
+
     }
 
     public function getTransactions(){
-        $pdo = new PDO('mysql:host=database;dbname=test', 'andrea', 'ciaone');
-        $statement = $pdo->query("SELECT 
-        transactions.id as tid,
-        transactions.user_id as uid,
-        transactions.amount as amount,
-        transactions.currency as currency,
-        products.id as pid,
-        products.description as description
-        from transactions, products where transactions.product_id = products.id");
-        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $objects = array_map('toDto', $rows);
-        return json_encode($objects);
+        try {
+            $pdo = new PDO('mysql:host=database;dbname=test', 'andrea', 'ciaone');
+            $statement = $pdo->query("SELECT 
+              transactions.id as tid,
+              transactions.user_id as uid,
+              transactions.amount as amount,
+              transactions.currency as currency,
+              products.id as pid,
+              products.description as description
+              from transactions, products where transactions.product_id = products.id");
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $objects = array_map('toDto', $rows);
+            return json_encode($objects);
+        }
+        catch(Exception $e) {
+            throw new Exception("Error");
+        }
     }
 
     public function deleteTransaction($id) {
-        $pdo = new PDO('mysql:host=database;dbname=test', 'andrea', 'ciaone');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE FROM transactions WHERE id=" . $id;
-        $pdo->exec($sql);
-        return json_encode([]);
+        try {
+            $pdo = new PDO('mysql:host=database;dbname=test', 'andrea', 'ciaone');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "DELETE FROM transactions WHERE id=" . $id;
+            $pdo->exec($sql);
+            return json_encode([]);
+        }
+        catch(Exception $e) {
+            throw new Exception("Error");
+        }
     }
 }
 
@@ -37,7 +47,7 @@ function toDto($elem)
             'id' => $elem['pid'],
             'description' => $elem['description']
         ],
-        'amount' => $elem['amount'],
+        'amount' => round($elem['amount'], 2),
         'currency' => $elem['currency']
     ];
 
